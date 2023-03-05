3b8f07eb835c00a551d2c880653b7fdbed319373
hijunglit <hijunglit@gmail.com>
2023-03-05 14:01:37 +0900

    로컬 스토리지에 to do item 배열로  저장

--- js/todo.js
+++ js/todo.js
@@ -1,16 +1,42 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
-
+let toDoList = [];
 
 todoForm.addEventListener("submit", handleToDoSubmit);
+
 function handleToDoSubmit(event) {
     event.preventDefault();
-    
-    const toDos = todoInput.value;
-    paintTodo();
+    paintToDo();
+    handleToDo();
+    todoInput.value = '';
 }
 
-    function paintTodo() {
-        todoList.innerText = toDos;
+function paintToDo() {
+    let toDos = todoInput.value;
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    todoList.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText = toDos;
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+}
+
+function deleteToDo(event) {
+    const targetTodo = event.target.parentElement;
+    targetTodo.remove();
+}
+
+function handleToDo() {
+    let toDos = todoInput.value;
+    toDoList.push(toDos);
+    JSON.stringify(toDoList);
+    localStorage.setItem("todo", JSON.stringify(toDoList));
+}
+
+function deleteItemToLocalStorage() {
+
 }
